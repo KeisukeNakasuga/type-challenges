4fdf01aa9ebc3e8b1e05edf3261d5767637f6f98
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-10-28 04:20:44 +0900

    feat: 0043解答追加

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,5 @@
-type MyExclude<T, U> = any
+// type MyExclude<T extends , U extends string | number> = {
+//   [_ in T]: T extends U ? never : T
+// }[T]
+
+type MyExclude<T, U> = T extends U ? never : T;
