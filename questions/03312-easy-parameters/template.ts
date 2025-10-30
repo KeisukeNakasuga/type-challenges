0ad178e05d54b1c52097f1cd94b54c8d23f06168
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-10-31 02:46:29 +0900

    feat: 03312解答追加

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,7 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> =   // Tは関数型である
+  T extends (...any: infer S) => any  // Tが関数の形であることを確認 & inferで引数の型を推論 
+    ? S  // 推論できる形であれば、それをそのまま結果にする
+    : never
+
+function foo(arg1: string, arg2: number): void {}
+type f = typeof foo; 
