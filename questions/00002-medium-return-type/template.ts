1f2ef5d03bcad0d6fb308974751b3ebc95126795
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-11-01 00:19:26 +0900

    feat: 00002解答追加

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,21 @@
-type MyReturnType<T> = any
+// my solution
+// type MyReturnType<T extends (...args: any[]) => any> =
+//   T extends (...any: infer _) => infer U
+//     ? U
+//     : never
+
+type MyReturnType<T extends Function> =
+  T extends (...args: any) => infer R
+    ? R
+    : never
+
+const fn = (v: boolean) => {
+  if (v)
+    return 1
+  else
+    return 2
+}
+
+type a = typeof fn
+
+type t1 = MyReturnType<typeof fn>;
