2d6f78e823478e283eceb9fa4a152355f7a1df13
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-11-01 01:00:30 +0900

    feat: 00003解答追加

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,11 @@
-type MyOmit<T, K> = any
+// type MyOmit<T, K extends keyof T> = {
+//   [P in keyof T as key extends K ? never : key]: T[key];
+// }
+
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as
+    P extends K
+      ? never
+      : P
+  ]: T[P]
+}
