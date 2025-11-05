2eaa6cb452269f32fb8b7eaae21eb161e551281b
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-11-06 07:44:19 +0900

    feat: 00010解答追加

--- questions/00010-medium-tuple-to-union/template.ts
+++ questions/00010-medium-tuple-to-union/template.ts
@@ -1,1 +1,4 @@
-type TupleToUnion<T> = any
+type TupleToUnion<T extends ReadonlyArray<unknown>> = T[number]
+
+const arr_00010 = [1, 'foo', true] as const
+type test_00010_1 = ReadonlyArray<[1, 'foo', true]>
