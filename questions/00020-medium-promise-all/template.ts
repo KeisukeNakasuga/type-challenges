4af6496eae316f2eb8e7f0c27f7066e66fe02459
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2025-11-20 13:39:20 +0900

    feat: 20 解答追加

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,17 @@
-declare function PromiseAll(values: any): any
+declare function PromiseAll<T extends unknown[]>(values: readonly [...T]): 
+  Promise<{ [key in keyof T]: Awaited<T[key]> }>
+
+// 1. Tはなんでも入る配列
+// 2. valuesにはTの各要素を展開
+//    readonlyにするのは
+
+
+function identityNoReadonly<T extends unknown[]>(
+  values: readonly [...T]
+): T {
+  return values as T;
+}
+
+const data = [10, 'apple'] as const;
+
+const res = identityNoReadonly(data);
